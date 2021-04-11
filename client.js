7c136707454b280213ab0233d9118be286602266
millken <millken@gmail.com>
2021-04-11 19:42:22 +0800

    grpc server

--- client.js
+++ client.js
@@ -11,6 +11,63 @@ let proto = grpc.loadPackageDefinition(
       })
 );
 
-  const REMOTE_SERVER = "0.0.0.0:2019";
+  const REMOTE_SERVER = "0.0.0.0:2021";
 let client = new proto.solc.CompilerService(REMOTE_SERVER, grpc.credentials.createInsecure());
-client.solcCompiler({language: "Solidity"}, (err, res) =>{ console.log(res); } );
+let compilerInput = {
+  version: "0.5.7+commit.6da8b019",
+  language: "Solidity",
+  //settings:{},
+  sources: [
+    {
+      name: "hello.sol",
+      content: `// SPDX-License-Identifier: MIT
+
+      pragma solidity >=0.5.0 <0.9.0;
+      
+      contract HelloWorld {
+          string defaultName;
+          mapping(address => string) public accounts;
+      
+          constructor() public {
+              defaultName = "World";
+          }
+      
+          function getMessage() public view returns (string memory) {
+              string memory name = bytes(accounts[msg.sender]).length > 0
+                  ? accounts[msg.sender]
+                  : defaultName;
+              return concat("Hello", name);
+          }
+      
+          function setName(string memory name) public returns (bool success) {
+              require(bytes(name).length > 0);
+              accounts[msg.sender] = name;
+              return true;
+          }
+      
+          function concat(string memory _base, string memory _value)
+              internal
+              pure
+              returns (string memory)
+          {
+              bytes memory _baseBytes = bytes(_base);
+              bytes memory _valueBytes = bytes(_value);
+              string memory _tmpValue = new string(
+                  _baseBytes.length + _valueBytes.length
+              );
+              bytes memory _newValue = bytes(_tmpValue);
+              uint256 i;
+              uint256 j;
+              for (i = 0; i < _baseBytes.length; i++) {
+                  _newValue[j++] = _baseBytes[i];
+              }
+              for (i = 0; i < _valueBytes.length; i++) {
+                  _newValue[j++] = _valueBytes[i];
+              }
+              return string(_newValue);
+          }
+      }`
+    },
+  ],
+}
+client.solcCompiler(compilerInput, (err, res) =>{ console.log(res); } );
--- server.js
+++ server.js
@@ -1,25 +1,114 @@
+#!/usr/bin/env node
 let grpc = require("grpc");
 let protoLoader = require("@grpc/proto-loader");
- 
-const server = new grpc.Server();
-const URL = "0.0.0.0:2019";
+let solc = require('solc');
+let log4js = require("log4js");
+const program = require('commander');
 
-let proto = grpc.loadPackageDefinition(
-    protoLoader.loadSync("./proto/solc.proto",{
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
-      })
-);
+program
+  .version('0.1.0')
+  .option('-d, --debug', 'debug level', false)
+  .option('-l, --listen <address>', 'grpc server listen address', '0.0.0.0:2021')
+  .option('-b, --solcbin <folder>', 'directory with solc json bin files', process.cwd() + '/solc-bin/')
+  .action(runServer);
+program.parse();
 
-function solcCompiler(call, callBack) {
-       callBack(null, {content: `Hello ${call.request.language} welcome to the world`});
+let debug = program.opts().debug ? true : false;
+
+const logger = log4js.getLogger('sol-compiler-service', 'console');
+logger.level = debug ? "debug" : "info";
+console.debug = logger.debug.bind(logger);
+console.log = logger.info.bind(logger);
+console.warn = logger.warn.bind(logger);
+console.error = logger.error.bind(logger);
+console.trace = logger.trace.bind(logger);
+
+function runServer() {
+
+  let proto = grpc.loadPackageDefinition(
+    protoLoader.loadSync("./proto/solc.proto", {
+      keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true
+    })
+  );
+
+  let server = new grpc.Server();
+  server.addService(proto.solc.CompilerService.service, { solcCompiler: solcCompiler });
+  server.bind(program.opts().listen, grpc.ServerCredentials.createInsecure());
+  console.log("starting grpc server on: " + program.opts().listen);
+  server.start();
 }
 
-server.addService(proto.solc.CompilerService.service, { solcCompiler: solcCompiler });
+function formatFatalError(message) {
+  return JSON.stringify({
+    errors: [
+      {
+        'type': 'JSONError',
+        'component': 'solcjs',
+        'severity': 'error',
+        'message': message,
+        'formattedMessage': 'Error: ' + message
+      }
+    ]
+  });
+}
 
-server.bind(URL, grpc.ServerCredentials.createInsecure());
- 
-server.start();
+function moduleIsAvailable(path) {
+  try {
+    require.resolve(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function compilerResponse(content) {
+  return { content: content }
+}
+
+function solcCompiler(call, callBack) {
+  console.debug("grpc request :", call.request)
+  let version = call.request.version
+  if (version == "") {
+    console.error("missing version request");
+    callBack(null, compilerResponse(formatFatalError("version required")));
+    return;
+  }
+  let soljsonPath = program.opts().solcbin + "/soljson-v" + version + ".js";
+  if (!moduleIsAvailable(soljsonPath)) {
+    console.error("soljson file not exist: " + soljsonPath);
+    callBack(null, compilerResponse(formatFatalError("module not found" + version)));
+    return;
+  }
+  let soljson = solc.setupMethods(require(soljsonPath));
+  let language = call.request.language || "Solidity";
+  let input = {
+    language: language,
+    sources: {},
+    settings: {
+      optimizer: {
+        enabled: false,
+        runs: 200,
+      },
+      outputSelection: {
+        '*': {
+          '*': ["abi", "metadata", "evm.bytecode", "evm.methodIdentifiers", "evm.gasEstimates"]
+        }
+      }
+    }
+  };
+  let optimizer = call.request.optimizer || { enabled: false, runs: 200 }
+  input.settings.optimizer.enabled = optimizer.enabled || false;
+  input.settings.optimizer.runs = optimizer.runs || 200;
+  let sources = call.request.sources || [];
+  for (var k in sources) {
+    input.sources[sources[k].name || ""] = { content: sources[k].content || "" };
+  }
+
+  let output = soljson.compile(JSON.stringify(input));
+  console.debug("grpc response :", output)
+  callBack(null, compilerResponse(output));
+}
